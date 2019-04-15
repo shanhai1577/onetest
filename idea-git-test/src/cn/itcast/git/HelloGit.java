8abd40a3b4ad0ecce6727ceb102d7fbf2eb476c8
shanhai <3488820935@qq.com>
2019-04-15 20:30:04 +0800

    第一次提交

---
+++ idea-git-test/src/cn/itcast/git/HelloGit.java
@@ -0,0 +1,12 @@
+package cn.itcast.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("从你的全世界路过");
+        System.out.println("从你的全世界路过");
+        System.out.println("从你的全世界路过");
+        System.out.println("从你的全世界路过");
+        System.out.println("从你的全世界路过");
+        System.out.println("从你的全世界路过");
+    }
+}
